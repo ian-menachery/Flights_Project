e03f2a79d94e23dd6c56855a24fc778965ee312e
William Hanvey <hanvey.w@northeastern.edu>
2025-02-18 23:06:31 -0500

    better bokeh chord

--- will_viz_testing.py
+++ will_viz_testing.py
@@ -26,23 +26,52 @@ plt.xlabel('Airport')
 plt.ylabel('Number of Airlines')
 plt.show()
 '''
+
 # Chord Diagram
 iata_to_country = df.set_index('Origin IATA')['Origin Country'].to_dict()
 df['Destination Country'] = df['Destination'].map(iata_to_country)
 df = df.dropna(subset=['Destination Country'])
 
 hv.extension('bokeh')
+
 country_routes = df.groupby(['Origin Country', 'Destination Country']).size().reset_index(name = 'Flights')
+country_routes = country_routes[country_routes['Origin Country'].isin(['Singapore', 'Malaysia', 'Thailand', 'Viet Nam'])]
+country_routes = country_routes[country_routes['Destination Country'].isin(['Singapore', 'Malaysia', 'Thailand', 'Viet Nam'])]
+country_routes = country_routes[country_routes['Destination Country'] != country_routes['Origin Country']]
 chord_data = [(row['Origin Country'], row['Destination Country'], row['Flights']) for _, row in country_routes.iterrows()]
 
-chord = hv.Chord(chord_data).opts(
+countries = ['Malaysia', 'Singapore',  'Thailand', 'Viet Nam']
+custom_colors = ['#354698', '#E62837', '#28224F', '#F4ED3C']
+
+nodes_df = pd.DataFrame({
+    'index': countries,
+    'name': countries,
+    'color': custom_colors
+})
+
+nodes = hv.Dataset(nodes_df, 'index')
+
+country_routes = df.groupby(['Origin Country', 'Destination Country']).size().reset_index(name='Flights')
+country_routes = country_routes[
+    country_routes['Origin Country'].isin(countries) &
+    country_routes['Destination Country'].isin(countries)
+]
+country_routes = country_routes[country_routes['Destination Country'] != country_routes['Origin Country']]
+
+chord_data = [
+    (row['Origin Country'], row['Destination Country'], row['Flights'])
+    for _, row in country_routes.iterrows()
+]
+chord_df = pd.DataFrame(chord_data, columns=['source', 'target', 'value'])
+
+chord = hv.Chord((chord_df, nodes)).opts(
     width=800, height=800,
     title="Country-to-Country Flight Connectivity",
-    cmap='Category20',
-    edge_color=dim('source').str(),
-    node_color=dim('index').str(),
-    labels='name'
+    node_color='color',
+    labels='name',
+    edge_color=dim('source'),
+    edge_line_width=dim('value'),
+    cmap=custom_colors
 )
 
-#chord
 show(hv.render(chord, backend='bokeh'))
