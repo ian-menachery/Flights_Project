06101ad634b1b724e28aebb17f25a5b4db40160b
William Hanvey <hanvey.w@northeastern.edu>
2025-03-29 13:17:36 -0400

    added few more elements that would benefit viz

--- airport_cords.py
+++ airport_cords.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 
 df = pd.read_csv('asia_routes.csv')
-df = df[['iata', 'country_code', 'latitude', 'longitude']]
+print(df.columns)
+df = df[['iata', 'name', 'country', 'city_name', 'latitude', 'longitude']]
 df.to_csv('airport_coordinates.csv')
 print(df.columns)
 print(df.head())
