ef32bd272fe9bb65354284334f7ac501b0ed7324
William Hanvey <hanvey.w@northeastern.edu>
2025-04-01 16:22:39 -0400

    added HK map

--- will_pythonviz.py
+++ will_pythonviz.py
@@ -2,6 +2,11 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 import folium
+import ipywidgets as widgets
+from IPython.display import display, clear_output
+
+
+### Viz 1
 
 df = pd.read_csv('Final_Asia-Only_Flight_Routes.csv')
 coordinate_df = pd.read_csv('airport_coordinates.csv')
@@ -42,3 +47,28 @@ for _, row in airport_data.iterrows():
     ).add_to(m)
 
 m.save('airport_bubble_map.html')
+
+### Viz 2 File Building
+
+df = pd.read_csv('Final_Asia-Only_Flight_Routes.csv')
+
+df = df.merge(df[['Origin', 'Origin Country']], 
+              left_on='Destination', 
+              right_on='Origin', 
+              how='left', 
+              suffixes=('', ' Destination'))
+
+df.rename(columns={'Origin Country Destination': 'Destination Country'}, inplace=True)
+df = df[['Origin', 'Origin Country', 'Destination', 'Destination Country']]
+df = df.drop_duplicates()
+
+airport_df = pd.read_csv("airport_coordinates.csv")
+airport_df.rename(columns={'iata': 'Origin', 'latitude': 'Latitude', 'longitude': 'Longitude'}, inplace=True)
+df = df.merge(airport_df[['Origin', 'Latitude', 'Longitude']], on='Origin', how='left')
+df.rename(columns={'Latitude': 'Origin Latitude', 'Longitude': 'Origin Longitude'}, inplace=True)
+airport_df.rename(columns={'Origin': 'Destination'}, inplace=True)
+df = df.merge(airport_df[['Destination', 'Latitude', 'Longitude']], on='Destination', how='left')
+df.rename(columns={'Latitude': 'Destination Latitude', 'Longitude': 'Destination Longitude'}, inplace=True)
+df = df.dropna()
+#df.to_csv('all_connections_coordinates.csv', index=False)
+print(df)
